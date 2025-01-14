a3c3e6a738c5210d64af5533d676a75690a069fd
Riku-mono <riitkou@gmail.com>
2025-01-14 14:20:59 +0900

    add MoviesView and API endpoint for fetching movie list

--- backend/api/online/views.py
+++ backend/api/online/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Movie
+from .mappers import MovieMapper
+from rest_framework import status
 
-# Create your views here.
+
+class MoviesView(APIView):
+    """映画リストビュークラス
+    """
+
+    def get(self, request, format=None):
+        """映画リストを取得する。
+
+        Returns
+        -------
+        movies_dict : Response
+            映画リスト
+        """
+        movies = Movie.objects.order_by('?')[:20]
+        movies_dict = [MovieMapper(movie).as_dict() for movie in movies]
+        return Response(movies_dict, status.HTTP_200_OK)
