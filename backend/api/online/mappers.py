55962dd3ef00473079ae7d5e9aad277adbe41fb4
Riku-mono <riitkou@gmail.com>
2025-01-14 14:11:35 +0900

    add MovieMapper class to convert movie objects to dictionary format

---
+++ backend/api/online/mappers.py
@@ -0,0 +1,16 @@
+class MovieMapper:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def as_dict(self):
+        movie = self.obj
+        genres = [genre.name for genre in movie.genres.all()]
+        
+        return {
+            'id': movie.id,
+            'title': movie.title,
+            'year': movie.year,
+            'genres': genres,
+            'imdb_id': movie.imdb_id,
+            'tmdb_id': movie.tmdb_id,
+        }
